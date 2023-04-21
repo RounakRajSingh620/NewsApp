74aeb84a37248d6ab0af03848a4427b0017c2c83
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2023-04-21 20:39:45 +0530

    News-App

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import './App.css';
 import React, { Component } from 'react'
 
 export default class App extends Component {
-  c = 'ROUNAK'
+  
   render() {
     return (
       <div>
-        Hello my first class based component {this.c}
+        
       </div>
     )
   }
