041c82c67bf0b986b75d01b064681479bda47140
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2023-04-23 11:12:56 +0530

    NewsApp

--- src/components/News.js
+++ src/components/News.js
@@ -2,17 +2,26 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
+  constructor() {
+    super();
+    console.log("Hello I am a constructor")
+  }
   render() {
     return (
       <div className="container my-3">
         <h2>NewsMonkey-Top Headlines</h2>
-        <NewsItem title="myTitle" description="mydesc" />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
+        <div className="row">
+          <div className="col-md-4">
+            <NewsItem title="myTitle" description="mydesc" imageUrl="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="myTitle" description="mydesc" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="myTitle" description="mydesc" />
+          </div>
 
+        </div>
       </div>
     )
   }
