5b742a598d1375e94c6bbb59e2a9fd9d2c857542
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2023-04-30 20:28:41 +0530

    NewsApp

--- src/components/News.js
+++ src/components/News.js
@@ -7,14 +7,15 @@ export class News extends Component {
     super();
     this.state = {
       articles: [],
-      loading: false
+      loading: false,
+      page: 1
     }
   }
   async componentDidMount() {
-    let url = "  https://newsapi.org/v2/top-headlines?country=in&apiKey=9bcc7f5567eb41a0a52c8885f755801b&page=1&pageSize=20";
+    let url = "  https://newsapi.org/v2/top-headlines?country=in&apiKey=9bcc7f5567eb41a0a52c8885f755801b&page=1pageSize=20";
     let data = await fetch(url);
     let parsedData = await data.json()
-    this.setState({ articles: parsedData.articles })
+    this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults })
   }
   handlePrevClick = async () => {
     console.log("Previous");
@@ -28,15 +29,19 @@ export class News extends Component {
   }
   handleNextClick = async () => {
     console.log("Next");
+    if (this.state.page + 1 > Math.ceil(this.state.totalResults)) {
 
-    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=9bcc7f5567eb41a0a52c8885f755801b&page=${this.state.page + 1}&pageSize=20`;
-    let data = await fetch(url);
-    let parsedData = await data.json()
-    this.setState({
-      page: this.state.page + 1,
-      articles: parsedData.articles
-    })
+    }
+    else {
 
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=9bcc7f5567eb41a0a52c8885f755801b&page=${this.state.page + 1}&pageSize=20`;
+      let data = await fetch(url);
+      let parsedData = await data.json()
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsedData.articles
+      })
+    }
   }
   render() {
     return (
@@ -52,8 +57,8 @@ export class News extends Component {
 
         </div>
         <div className="container d-flex justify-content-between">
-          <button disabled={this.state.page <= 1} type="button" class="btn btn-dark" onClick={this.handlePrevClick}> &larr; Previous</button>
-          <button type="button" class="btn btn-dark" onClick={this.handleNextClick}> Next &rarr;</button>
+          <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}> &larr; Previous</button>
+          <button type="button" className="btn btn-dark" onClick={this.handleNextClick}> Next &rarr;</button>
         </div>
       </div>
     )
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,7 +6,7 @@ export class NewsItem extends Component {
     return (
       <div className='my-3'>
         <div className="card" style={{ width: "18rem" }}>
-          <img src={!imageUrl ? "https://www.hindustantimes.com/ht-img/img/2023/04/26/1600x900/indian_high_commission_london_1682516237331_1682516237509.jpeg" : imageUrl} className="card-img-top" alt="..." />
+          <img src={!imageUrl ? "https://th.bing.com/th/id/R.ca0bc1b3b2ae4c237f5ecffeaa442181?rik=EkBYtdEYSIVgFg&riu=http%3a%2f%2fwonderfulengineering.com%2fwp-content%2fuploads%2f2014%2f01%2fbeautiful-wallpaper-39.jpg&ehk=dUX8Qlz0ClF18r8HQj8I6vVmKHhdPGv2kxUu3fCR15c%3d&risl=&pid=ImgRaw&r=0" : imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
