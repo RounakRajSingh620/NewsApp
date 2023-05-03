5d164ffb7843d06df2b867d63fe043f528b88348
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2023-05-03 20:12:24 +0530

    NewsApp

--- src/components/News.js
+++ src/components/News.js
@@ -48,7 +48,7 @@ export class News extends Component {
       <div className="container my-3">
         <h1 className='text-center'>NewsMonkey-Top Headlines</h1>
         <div className="row">
-          {this.state.articles.flatMap((element) => {
+          {this.state.articles.map((element) => {
             return <div className="col-md-4" key={element.url}>
               <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} />
             </div>
