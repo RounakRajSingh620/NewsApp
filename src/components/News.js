c548a3d3667c7e7062afd1d4fc0cdf2f47fa653b
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2023-05-07 19:39:56 +0530

    NewsApp

--- src/components/News.js
+++ src/components/News.js
@@ -66,7 +66,7 @@ export class News extends Component {
         <h1 className='text-center'>NewsMonkey-Top Headlines</h1>
         {this.state.loading && <Spinner />}
         <div className="row">
-          {this.state.loading && this.state.articles.map((element) => {
+          {!this.state.loading && this.state.articles.map((element) => {
             return <div className="col-md-4" key={element.url}>
               <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} />
             </div>
