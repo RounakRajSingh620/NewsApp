19ed16ce9d50c6981195262fa605f2eba463393d
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2023-05-11 22:36:55 +0530

    NewsApp

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,7 +9,7 @@ export class NavBar extends Component {
             
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                     <div className="container-fluid">
-                        < className="navbar-brand" href ="/">Navbar
+                        <Link className="navbar-brand" href ="/">Navbar</Link>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
