f7e13cdfe5b97b581d0423f0e01b2b2d96f0638d
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2023-05-12 20:38:26 +0530

    NewsApp

--- src/App.js
+++ src/App.js
@@ -17,14 +17,14 @@ export default class App extends Component {
         <Router>
           <NavBar />
           <Switch>
-            <Route path='/'><News pageSize={5} country="in" category="general" /></Route>
-            <Route path='/business'><News pageSize={5} country="in" category="business" /></Route>
-            <Route path='/entertainment'><News pageSize={5} country="in" category="entertainment" /></Route>
-            <Route path='/general'><News pageSize={5} country="in" category="general" /></Route>
-            <Route path='/health'><News pageSize={5} country="in" category="" /></Route>
-            <Route path='/science'><News pageSize={5} country="in" category="science" /></Route>
-            <Route path='/sportstechnology'><News pageSize={5} country="in" category="sportstechnology" /></Route>
-            <Route path='/technology'><News pageSize={5} country="in" category="technology" /></Route>
+            <Route exact path='/'><News key="general" pageSize={5} country="in" category="general" /></Route>
+            <Route exact path='/business'><News key="business'" pageSize={5} country="in" category="business" /></Route>
+            <Route exact path='/entertainment'><News key="entertainment" pageSize={5} country="in" category="entertainment" /></Route>
+            <Route exact path='/general'><News key="general" pageSize={5} country="in" category="general" /></Route>
+            <Route exact path='/health'><News key="health" pageSize={5} country="in" category="" /></Route>
+            <Route exact path='/science'><News key="science" pageSize={5} country="in" category="science" /></Route>
+            <Route exact path='/sports'><News key="sports" pageSize={5} country="in" category="sportstechnology" /></Route>
+            <Route exact path='/technology'><News key="technology" pageSize={5} country="in" category="technology" /></Route>
           </Switch>
         </Router>
       </div>
