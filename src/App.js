dad791509efa61217895df611bb259f70e55e560
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2023-05-13 20:13:35 +0530

    NewsApp

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 import React, { Component } from 'react'
 import NavBar from './components/NavBar';
-import {News} from './components/News';
+import { News } from './components/News';
 import {
   BrowserRouter as Router,
   Route,
@@ -17,8 +17,8 @@ export default class App extends Component {
         <Router>
           <NavBar />
           <Switch>
-            <Route exact path='/'><News key="general" pageSize={5} country="in" category="general" /></Route>
-            <Route exact path='/business'><News key="business'" pageSize={5} country="in" category="business" /></Route>
+            <Route exact path="/">"<News key="general" pageSize={5} country="in" category="general" /></Route>
+            <Route exact path="/business"><News key="business'" pageSize={5} country="in" category="business" /></Route>
             <Route exact path='/entertainment'><News key="entertainment" pageSize={5} country="in" category="entertainment" /></Route>
             <Route exact path='/general'><News key="general" pageSize={5} country="in" category="general" /></Route>
             <Route exact path='/health'><News key="health" pageSize={5} country="in" category="" /></Route>
